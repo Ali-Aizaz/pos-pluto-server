83cf58b9c117f80285789c386df0e1268f52c55b
Ali-Aizaz <50923380+Ali-Aizaz@users.noreply.github.com>
2023-05-01 10:43:01 +0500

    fixed issues with saveImage and emailLimiter

--- utils/emailLimiter.js
+++ utils/emailLimiter.js
@@ -1,4 +1,5 @@
 const rateLimit = require('express-rate-limit');
+
 const emailLimiter = rateLimit({
   windowMs: 60 * 60 * 1000, // 1 hour
   max: 5, // Limit each IP to send 5 emails per hour
--- utils/saveImage.js
+++ utils/saveImage.js
@@ -2,6 +2,19 @@ const crypto = require('crypto');
 const fs = require('fs');
 const path = require('path');
 
+const decodeBase64Image = (dataString) => {
+  const matches = dataString.match(/^data:([A-Za-z-+/]+);base64,(.+)$/);
+  const response = {};
+  if (matches?.length !== 3) {
+    return new Error('Invalid input string');
+  }
+  // eslint-disable-next-line prefer-destructuring
+  response.type = matches[1];
+  // eslint-disable-next-line new-cap
+  response.data = new Buffer.from(matches[2], 'base64');
+  return response;
+};
+
 const saveImage = (image, folder, oldImageUrl) => {
   const directory = path.join(__dirname, '..', `static/${folder}/`);
   const imageTypeRegularExpression = /\/(.*?)$/;
@@ -11,37 +24,26 @@ const saveImage = (image, folder, oldImageUrl) => {
 
   const imageBuffer = decodeBase64Image(image);
   const imageTypeDetected = imageBuffer.type?.match(imageTypeRegularExpression);
-  let filename = uniqueSHA1String + '.';
-  if (imageTypeDetected !== undefined)
-    filename = filename + imageTypeDetected[1];
+  let filename = `${uniqueSHA1String}.`;
+  if (imageTypeDetected !== undefined) filename += imageTypeDetected[1];
 
   fs.mkdirSync(directory, { recursive: true });
   if (imageBuffer.data !== undefined)
     fs.writeFileSync(directory + filename, imageBuffer.data);
 
-  const imagePath =
-    process.env.SERVER_ROOT_URI + `/static/${folder}/` + filename;
+  const imagePath = `${process.env.SERVER_ROOT_URI}/static/${folder}/${filename}`;
 
   if (oldImageUrl) {
     try {
       const oldImage = oldImageUrl.split('/').at(-1);
       console.log(oldImage);
       fs.unlinkSync(directory + oldImage);
-    } catch {}
+    } catch (e) {
+      console.log(e);
+    }
   }
 
   return imagePath;
-};
-
-const decodeBase64Image = (dataString) => {
-  const matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
-  const response = {};
-  if (matches?.length !== 3) {
-    return new Error('Invalid input string');
-  }
-  response.type = matches[1];
-  response.data = new Buffer.from(matches[2], 'base64');
-  return response;
 };
 
 exports.saveImage = saveImage;
